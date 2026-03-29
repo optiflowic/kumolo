dfca0fe9270c8bd9d5336de937c172606d62f41f
Hisanari Kikuchi <hisanari.kikuchi.dev@gmail.com>
2026-03-30 00:47:45 +0900

    [Task]: Set up CI workflow (build, test, lint) (#27)
    * chore: add dev tools via go tool directive (golangci-lint, gosec, golines, goimports)
    
    * chore: update Makefile to use go tool and add install target
    
    * ci: add GitHub Actions CI workflow
    
    * fix: use http.Server with timeouts to resolve gosec G114
    
    * ci: fix step emoji for run all checks
    
    * chore: run go mod tidy
    
    * ci: add go mod tidy check

--- cmd/kumolo/main.go
+++ cmd/kumolo/main.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
 	addr := ":4566"
+	srv := &http.Server{
+		Addr:         addr,
+		Handler:      mux,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
 	fmt.Printf("kumolo listening on %s\n", addr)
-	log.Fatal(http.ListenAndServe(addr, mux))
+	log.Fatal(srv.ListenAndServe())
 }
