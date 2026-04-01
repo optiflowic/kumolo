18b129f1f715e7fe94dea5567cf492a3b399e637
Hisanari Kikuchi <hisanari.kikuchi.dev@gmail.com>
2026-04-02 00:01:34 +0900

    feat(logging): add HTTP request/response logging middleware
    Log method, path, status code, and duration for every request.

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,15 +3,16 @@ package server
 import (
 	"net/http"
 
+	"github.com/optiflowic/kumolo/internal/logging"
 	"github.com/optiflowic/kumolo/internal/s3"
 )
 
-func NewMux(dataDir string) (*http.ServeMux, func(), error) {
+func NewMux(dataDir string) (http.Handler, func(), error) {
 	storage, err := s3.NewStorage(dataDir)
 	if err != nil {
 		return nil, nil, err
 	}
 	mux := http.NewServeMux()
 	mux.Handle("/", s3.NewRouter(storage))
-	return mux, func() { _ = storage.Close() }, nil
+	return logging.Middleware(mux), func() { _ = storage.Close() }, nil
 }
