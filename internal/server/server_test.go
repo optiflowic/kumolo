b3d2b8dee83a2edd6af7a8bf79cb06ba14c24c4e
Hisanari Kikuchi <hisanari.kikuchi.dev@gmail.com>
2026-04-04 01:34:21 +0900

    test(server): add error path test for NewMux

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -3,11 +3,24 @@ package server
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func TestNewMuxError(t *testing.T) {
+	// Place a file where NewStorage expects to create a directory.
+	dir := t.TempDir()
+	require.NoError(t, os.WriteFile(filepath.Join(dir, "s3"), []byte{}, 0o600))
+
+	mux, cleanup, err := NewMux(dir)
+	assert.Error(t, err)
+	assert.Nil(t, mux)
+	assert.Nil(t, cleanup)
+}
 
 func TestNewMux(t *testing.T) {
 	mux, cleanup, err := NewMux(t.TempDir())
